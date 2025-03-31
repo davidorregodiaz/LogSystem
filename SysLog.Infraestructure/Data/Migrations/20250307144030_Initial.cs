f0d480a64fdaaf069620103cab6d96e12b22815c
David Orrego <orregod091@gmail.com>
2025-03-30 23:47:59 -0400

    {ADD} CatchLogsService.cs backgroundService para capturar logs

---
+++ SysLog.Infraestructure/Data/Migrations/20250307144030_Initial.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SysLog.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Log",
+                columns: table => new
+                {
+                    LogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Acction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Interface = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Protocol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IpOut = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IpDestiny = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Signature = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Log", x => x.LogId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Log");
+        }
+    }
+}
